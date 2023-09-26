1836cfa2d62b74c5ca86c0809dd21d6fb4f1761e
aaron tanjaya <aaront.tjy@gmail.com>
2023-09-26 21:30:14 +0700

    feat: add mapper helper functions for data points, benchmarks, and fund detail psrdana

--- collector/mapper.go
+++ collector/mapper.go
@@ -7,6 +7,84 @@ import (
 	"github.com/aarontanjaya/finance-data-collector/entity"
 )
 
+func MapPasardanaFundDetail(r dto.PasardanaFundDetailResponse) (*entity.FundDetail, error) {
+	var res *entity.FundDetail
+
+	category := GetPasardanaMappedCategory(r.Type)
+	NAV, err := MapPasardanaDataPoint(r.NAVTimeSeries)
+	if err != nil {
+		return res, err
+	}
+
+	AUM, err := MapPasardanaDataPoint(r.AUM)
+	if err != nil {
+		return res, err
+	}
+
+	totalUnits, err := MapPasardanaDataPoint(r.TotalUnits)
+	if err != nil {
+		return res, err
+	}
+
+	benchmarks, err := MapPasardanaBenchmarks(r.Benchmarks)
+	if err != nil {
+		return res, err
+	}
+
+	res = &entity.FundDetail{
+		Source:            SourceIdPasardana,
+		Id:                r.Id,
+		Name:              r.Name,
+		Type:              category,
+		Currency:          r.Currency,
+		IsSharia:          r.IsSharia,
+		IsETF:             r.IsETF,
+		IsIndex:           r.IsIndex,
+		HasDividend:       r.HasDividend,
+		IMId:              r.IMId,
+		CustodianBankId:   r.CustodianBankId,
+		CustodianBankName: r.CustodianBankName,
+		Benchmarks:        benchmarks,
+		NAVTimeSeries:     NAV,
+		AUM:               AUM,
+		TotalUnits:        totalUnits,
+	}
+
+	return res, nil
+}
+
+func MapPasardanaDataPoint(r []dto.PasardanaDataPoint) ([]entity.DataPoint, error) {
+	result := make([]entity.DataPoint, len(r))
+	loc, _ := time.LoadLocation("Asia/Jakarta")
+	for idx, item := range r {
+		date, err := time.ParseInLocation("2006-01-02T15:04:05", item.Date, loc)
+		if err != nil {
+			return nil, err
+		}
+
+		result[idx].Date = uint64(date.Unix())
+		result[idx].Rate = item.Rate
+		result[idx].Value = item.Value
+	}
+
+	return result, nil
+}
+
+func MapPasardanaBenchmarks(r []dto.PasardanaFundBenchmark) ([]entity.FundBenchmark, error) {
+	result := make([]entity.FundBenchmark, len(r))
+	for idx, item := range r {
+		dp, err := MapPasardanaDataPoint(item.Data)
+		if err != nil {
+			return result, err
+		}
+		result[idx].Name = item.Name
+		result[idx].Id = item.Id
+		result[idx].Data = dp
+	}
+
+	return result, nil
+}
+
 func MapPasardanaFundSnp(r []dto.PasardanaSnapshotResponse) ([]entity.FundSnapshot, error) {
 	result := make([]entity.FundSnapshot, len(r))
 
